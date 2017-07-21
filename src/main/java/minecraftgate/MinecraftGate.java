4c792a72973307bccb20d950508cb26eea93147e
张振衣 <413149044@qq.com>
2017-07-21 22:16:37 +0800

    clean code and change name

--- src/main/java/minecraftgate/MinecraftGate.java
+++ src/main/java/minecraftgate/MinecraftGate.java
@@ -1,15 +1,14 @@
-package com.example.examplemod;
+package minecraftgate;
 
-import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
-public class ExampleMod
+@Mod(modid = MinecraftGate.MODID, version = MinecraftGate.VERSION)
+public class MinecraftGate
 {
-    public static final String MODID = "examplemod";
-    public static final String VERSION = "1.0";
+    public static final String MODID = "minecraftgate";
+    public static final String VERSION = "1.0.0";
     
     @EventHandler
     public void init(FMLInitializationEvent event)
