fe83b7cf8a95275c2bb3601063ad142dafa74be2
张振衣 <413149044@qq.com>
2017-07-27 20:21:40 +0800

    I changed name and delete all the things maybe unrelavent, it can run now.

---
+++ src/main/java/minecraftgate/MinecraftGate.java
@@ -0,0 +1,36 @@
+package minecraftgate;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+
+@Mod(modid = MinecraftGate.MODID, version = MinecraftGate.VERSION)
+public class MinecraftGate
+{
+    public static final String MODID = "minecraftgate";
+    public static final String VERSION = "1.0.0";
+
+    @SidedProxy(clientSide="minecraftgate.ClientOnlyProxy", serverSide="minecraftgate.DedicatedServerProxy")
+    public static Proxy proxy;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        proxy.preInit();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        proxy.init();
+    }
+
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+        proxy.postInit();
+    }
+}
